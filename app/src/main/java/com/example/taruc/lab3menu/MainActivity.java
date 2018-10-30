34a34891e6c5a9208ac0cd8912e32ae1af59e6b2
KennethChua980201 <kenenthclc-pa16@student.tarc.edu.my>
2018-10-30 13:21:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/lab3menu/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.taruc.lab3menu;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewMessage;
+    private float size;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Obtain the size of Text View
+        textViewMessage=findViewById(R.id.textViewMessage);
+        size= textViewMessage.getTextSize();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id==R.id.action_increase)
+        {   size+=1;
+            textViewMessage.setTextSize(size);}
+        else
+            if(id==R.id.action_decrease)
+        {   size-=1;
+            textViewMessage.setTextSize(size);}
+
+        return super.onOptionsItemSelected(item);
+    }
+}
